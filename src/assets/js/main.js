cd54db324ea1a9962622899705b6f9d383e244a5
stillingdesign <mike@stillingdigital.com>
2021-09-08 09:25:09 -0500

    add GSAP and stylize footer

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,20 @@
 import barba from '@barba/core';
 import LocomotiveScroll from 'locomotive-scroll';
+import { gsap } from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { InertiaPlugin } from "gsap/InertiaPlugin";
+import { SplitText } from "gsap/SplitText";
 
+gsap.registerPlugin(ScrollToPlugin, InertiaPlugin, SplitText);
+
+
+// Init Locomotive
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true
 });
 
-// Menu
+// Menu Open
 const menuIcon = document.querySelector('.navBtnContainer');
 const mobileNav = document.querySelector('.mobileNav');
 menuIcon.addEventListener(`click`, function() {
@@ -18,3 +26,10 @@ menuIcon.addEventListener(`click`, function() {
        mobileNav.classList.add(`active`);
     }
 });
+
+// Footer Marquee
+const footerMarquee = document.querySelector('.footerMarqueeItem');
+const footerMarqueeTrack = document.querySelector('.footerMarqueeTrack');
+const marqueeItemWidth = footerMarquee.offsetWidth;
+const marqueeTL = gsap.timeline({repeat:-1, defaults:{ease:"none"}});
+marqueeTL.to(footerMarqueeTrack,{x:-marqueeItemWidth, duration:20});

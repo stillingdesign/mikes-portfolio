d7a2232b1392efbcae671e7c6eabdc3e6b3ef626
stillingdesign <mike@stillingdigital.com>
2021-09-10 08:08:26 -0500

    did some weird stuff with barba and locomotive that seemingly works

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,34 +7,114 @@ import { SplitText } from "gsap/SplitText";
 
 gsap.registerPlugin(ScrollToPlugin, InertiaPlugin, SplitText);
 
-// Init Locomotive
-const scroll = new LocomotiveScroll({
+const menuIcon = document.querySelector('.navBtnContainer');
+const mobileNav = document.querySelector('.mobileNav');
+
+let scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true
 });
 
-scroll.destroy()
-
-setTimeout(function(){
-    scroll.init()
-}, 100);
-
-// Menu Open
-const menuIcon = document.querySelector('.navBtnContainer');
-const mobileNav = document.querySelector('.mobileNav');
-menuIcon.addEventListener(`click`, function() {
-  if(this.classList.contains(`active`)) {
-       this.classList.remove(`active`);
-       mobileNav.classList.remove(`active`);
-  } else {
-       this.classList.add(`active`);
-       mobileNav.classList.add(`active`);
-    }
+barba.hooks.leave(() => {
 });
 
-// Footer Marquee
-const footerMarquee = document.querySelector('.footerMarqueeItem');
-const footerMarqueeTrack = document.querySelector('.footerMarqueeTrack');
-const marqueeItemWidth = footerMarquee.offsetWidth;
-const marqueeTL = gsap.timeline({repeat:-1, defaults:{ease:"none"}});
-marqueeTL.to(footerMarqueeTrack,{x:-marqueeItemWidth, duration:20});
+barba.hooks.beforeLeave(() => {
+    menuIcon.classList.remove(`active`);
+    mobileNav.classList.remove(`active`);
+});
+
+barba.hooks.enter(() => {
+    window.scrollTo(0, 0);
+    globalScripts();
+});
+
+barba.hooks.after(() => {
+    scroll.update();
+    globalScripts();
+});
+
+barba.init({
+    transitions: [{
+
+        once(data) {
+            window.addEventListener('load', (event) => {
+                globalScripts();
+            })
+        }
+
+    }]
+});
+
+
+
+// Global Scripts
+const globalScripts = function globalScripts() {
+
+    // Menu Toggle
+    menuIcon.addEventListener(`click`, function() {
+    if(this.classList.contains(`active`)) {
+        this.classList.remove(`active`);
+        mobileNav.classList.remove(`active`);
+    } else {
+        this.classList.add(`active`);
+        mobileNav.classList.add(`active`);
+        }
+    });
+
+    // Footer Marquee
+    const footerMarquee = document.querySelector('.footerMarqueeItem');
+    const footerMarqueeTrack = document.querySelector('.footerMarqueeTrack');
+    const marqueeItemWidth = footerMarquee.offsetWidth;
+    console.log(marqueeItemWidth);
+    const marqueeTL = gsap.timeline({repeat:-1, defaults:{ease:"none"}});
+    marqueeTL.to(footerMarqueeTrack,{x:-marqueeItemWidth, duration:20});
+
+}
+
+
+
+
+
+
+/* Global Scripts
+const globalScripts = function globalScripts() {
+
+
+
+    // Init Locomotive
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true
+    });
+
+    scroll.destroy()
+
+    setTimeout(function(){
+        scroll.init()
+    }, 100);
+
+
+
+    // Menu Toggle
+    menuIcon.addEventListener(`click`, function() {
+    if(this.classList.contains(`active`)) {
+        this.classList.remove(`active`);
+        mobileNav.classList.remove(`active`);
+    } else {
+        this.classList.add(`active`);
+        mobileNav.classList.add(`active`);
+        }
+    });
+
+
+
+    // Footer Marquee
+    const footerMarquee = document.querySelector('.footerMarqueeItem');
+    const footerMarqueeTrack = document.querySelector('.footerMarqueeTrack');
+    const marqueeItemWidth = footerMarquee.offsetWidth;
+    console.log(marqueeItemWidth);
+    const marqueeTL = gsap.timeline({repeat:-1, defaults:{ease:"none"}});
+    marqueeTL.to(footerMarqueeTrack,{x:-marqueeItemWidth, duration:20});
+
+}
+*/

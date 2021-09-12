a2ea2e90039775fac41cbe7134ec548cbf7f8bd6
stillingdesign <mike@stillingdigital.com>
2021-09-12 06:58:43 -0500

    add imagesLoaded and update locomotive

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,120 +1,94 @@
+var imagesLoaded = require('imagesloaded');
+
 import barba from '@barba/core';
 import LocomotiveScroll from 'locomotive-scroll';
 import { gsap } from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { InertiaPlugin } from "gsap/InertiaPlugin";
 import { SplitText } from "gsap/SplitText";
 
-gsap.registerPlugin(ScrollToPlugin, InertiaPlugin, SplitText);
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger, InertiaPlugin, SplitText);
 
-const scrollContainer = document.querySelector('.locomotive');
+
+
+// Global Variables
 const menuIcon = document.querySelector('.navBtnContainer');
 const mobileNav = document.querySelector('.mobileNav');
 
-let scroll = new LocomotiveScroll({
+
+
+// Locomotive Scroll
+const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true
 });
 
-barba.hooks.beforeLeave(() => {
-    menuIcon.classList.remove(`active`);
-    mobileNav.classList.remove(`active`);
+
+
+// Mobile Menu Toggle
+const menuToggle = function menuToggle() {
+    menuIcon.addEventListener(`click`, function() {
+        if(this.classList.contains(`active`)) {
+            this.classList.remove(`active`);
+            mobileNav.classList.remove(`active`);
+        } else {
+            this.classList.add(`active`);
+            mobileNav.classList.add(`active`);
+        }
+    });
+}
+
+menuToggle();
+
+
+
+// Footer Marquee
+const footerMarqueeAnimate = function footerMarqueeAnimate() {
+    const footerMarquee = document.querySelector('.footerMarqueeItem');
+    const footerMarqueeTrack = document.querySelector('.footerMarqueeTrack');
+    const marqueeItemWidth = footerMarquee.offsetWidth;
+    console.log(marqueeItemWidth);
+    const marqueeTL = gsap.timeline({repeat:-1, defaults:{ease:"none"}});
+    marqueeTL.to(footerMarqueeTrack,{x:-marqueeItemWidth, duration:20});
+}
+
+
+
+// Update after Images Load
+imagesLoaded( 'body', function() {
+    console.log('images have loaded');
+    scroll.update();
+    footerMarqueeAnimate();
 });
 
-barba.hooks.beforeEnter(() => {
-    scroll.scrollTo(0, 0);
-});
 
-barba.hooks.enter(() => {
-    globalScripts();
-});
-
+/* BARBA JS START */
 barba.hooks.after(() => {
     scroll.update();
-    globalScripts();
 });
+
+
 
 barba.init({
+
     transitions: [{
+        name: 'default',
 
-        once(data) {
-            window.addEventListener('load', (event) => {
-                globalScripts();
-            })
-        }
+        once() {},
 
-    }]
+        leave({ current, next, trigger }) {},
+
+        enter({ current, next, trigger }) {
+            imagesLoaded( 'body', function() {
+                console.log('images have loaded');
+                scroll.update();
+            });
+        },
+
+    }],
+
+    views: [{}],
+
+    debug: true
 });
-
-
-// Global Scripts
-const globalScripts = function globalScripts() {
-
-    // Menu Toggle
-    menuIcon.addEventListener(`click`, function() {
-    if(this.classList.contains(`active`)) {
-        this.classList.remove(`active`);
-        mobileNav.classList.remove(`active`);
-    } else {
-        this.classList.add(`active`);
-        mobileNav.classList.add(`active`);
-        }
-    });
-
-    // Footer Marquee
-    const footerMarquee = document.querySelector('.footerMarqueeItem');
-    const footerMarqueeTrack = document.querySelector('.footerMarqueeTrack');
-    const marqueeItemWidth = footerMarquee.offsetWidth;
-    console.log(marqueeItemWidth);
-    const marqueeTL = gsap.timeline({repeat:-1, defaults:{ease:"none"}});
-    marqueeTL.to(footerMarqueeTrack,{x:-marqueeItemWidth, duration:20});
-
-}
-
-
-
-
-
-
-/* Global Scripts
-const globalScripts = function globalScripts() {
-
-
-
-    // Init Locomotive
-    const scroll = new LocomotiveScroll({
-        el: document.querySelector('[data-scroll-container]'),
-        smooth: true
-    });
-
-    scroll.destroy()
-
-    setTimeout(function(){
-        scroll.init()
-    }, 100);
-
-
-
-    // Menu Toggle
-    menuIcon.addEventListener(`click`, function() {
-    if(this.classList.contains(`active`)) {
-        this.classList.remove(`active`);
-        mobileNav.classList.remove(`active`);
-    } else {
-        this.classList.add(`active`);
-        mobileNav.classList.add(`active`);
-        }
-    });
-
-
-
-    // Footer Marquee
-    const footerMarquee = document.querySelector('.footerMarqueeItem');
-    const footerMarqueeTrack = document.querySelector('.footerMarqueeTrack');
-    const marqueeItemWidth = footerMarquee.offsetWidth;
-    console.log(marqueeItemWidth);
-    const marqueeTL = gsap.timeline({repeat:-1, defaults:{ease:"none"}});
-    marqueeTL.to(footerMarqueeTrack,{x:-marqueeItemWidth, duration:20});
-
-}
-*/
